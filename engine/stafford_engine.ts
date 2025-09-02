fe11b76cc648f2a3a052534c291fc25c43a61255
CodeItQuick <evanontario@hotmail.com>
2025-09-02 10:28:48 -0300

    stafford engine knows a trap

--- engine/stafford_engine.ts
+++ engine/stafford_engine.ts
@@ -1,19 +1,35 @@
 ﻿import type {MoveEvent} from "vue3-chessboard";
 
-let staffordEngine: { blackMoves: string[], whiteMoves: string[] } = { blackMoves: [], whiteMoves: [] };
-function staffordGameEngine() {
-    const mainRefutationBlack = ['e5', 'Nf6', 'Nc6', 'dxc6', 'Bc5', 'Ng4', 'Qh4', 'Qxg4', 'Bxg4']
-    const mainRefutationWhite = ['e4', 'Nf3', 'Nxe5', 'Nxc6', 'd3', 'Be2', 'Bxg4', 'g3', 'Qxg4']
+const staffordGameEngine = (refutationRoll = 1.0) => {
+    const mainRefutationBlack = ['e5', 'Nf6', 'Nc6', 'dxc6', 'Bc5', 'Ng4', 'Qh4', 'Qxg4', 'Bxg4'];
+    const mainRefutationWhite = ['e4', 'Nf3', 'Nxe5', 'Nxc6', 'd3', 'Be2', 'Bxg4', 'g3', 'Qxg4'];
+    const ohNoMyQueenBlack = ['e5', 'Nf6', 'Nc6', 'dxc6', 'Bc5', 'Nxe4', 'Bxf2', 'Bg4'];
+    const ohNoMyQueenWhite = ['e4', 'Nf3', 'Nxe5', 'd3', 'Be2', 'Bg5', 'Bxd8', 'Ke2'];
 
-    staffordEngine = { blackMoves: mainRefutationBlack, whiteMoves: mainRefutationWhite };
+    let staffordEngine;
+    if (refutationRoll > 0.5) {
+        staffordEngine = { blackMoves: mainRefutationBlack, whiteMoves: mainRefutationWhite };
+    } else {
+        staffordEngine = { blackMoves: ohNoMyQueenBlack, whiteMoves: ohNoMyQueenWhite };
+    }
 
     function determineWhiteNextMove(history: MoveEvent[]) {
         let currentBlackLine = history.filter((item) => item.color === 'b').map(item => item.san);
-        let engineMoveSoFar = staffordEngine.blackMoves.slice(0, currentBlackLine.length);
+        // play main refutation
+        if (refutationRoll > 0.5 && mainRefutationBlack.filter((engine, idx) => engine === currentBlackLine[idx]).length
+            === currentBlackLine.length) {
+            return mainRefutationWhite.shift();
+        }
+        let engineMoveSoFar = ohNoMyQueenBlack.slice(0, currentBlackLine.length);
+        // play fell for trap "Oh No My Queen"
+        if (refutationRoll <= 0.5 && ohNoMyQueenBlack.filter((engine, idx) => engine === currentBlackLine[idx]).length
+            === currentBlackLine.length) {
+            return ohNoMyQueenWhite.shift();
+        }
+
+        // play vs computer
         if (engineMoveSoFar.some((engine, idx) => engine !== currentBlackLine[idx])) {
             console.log(`You are off the main line! ${staffordEngine.blackMoves[0]} !== ${currentBlackLine[0]}`)
-        } else {
-            return staffordEngine.whiteMoves.shift();
         }
     }
     return { ...staffordEngine, determineWhiteNextMove };
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,9 +1,222 @@
 ﻿import assert from "node:assert";
+import {staffordGameEngine} from "../engine/stafford_engine";
+import type { MoveEvent } from "vue3-chessboard";
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
+describe('knows the main refutation', function () {
+    it('plays e4 on first move', function () {
+        const staffordEngine = staffordGameEngine();
+        const moves: MoveEvent[] = [];
+
+        const firstMove = staffordEngine.determineWhiteNextMove(moves)
+
+        assert.equal(firstMove, "e4")
+    });
+    it('plays Nf3 on second move', function () {
+        const staffordEngine = staffordGameEngine();
+        const moves: MoveEvent[] = [
+            {
+                after: "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",
+                before: "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
+                color: "b",
+                flags: "w",
+                from: "e7",
+                lan: "e2e4",
+                piece: "p",
+                san: "e5",
+                to: "e5",
+            } as MoveEvent
+        ];
+        staffordEngine.determineWhiteNextMove([]);
+
+        const firstMove = staffordEngine.determineWhiteNextMove(moves)
+
+        assert.equal(firstMove, "Nf3")
+    });
+    it('plays Be2 on seventh move', function () {
+        const staffordEngine = staffordGameEngine();
+        const moves: MoveEvent[] = [
+            {
+                color: "b",
+                san: "e5"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Nf6"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Nc6"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "dxc6"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Bc5"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Ng4"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Qh4"
+            } as MoveEvent,
+        ];
+        staffordEngine.determineWhiteNextMove([]);
+        staffordEngine.determineWhiteNextMove([moves[0]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4]]);
+
+        const firstMove = staffordEngine.determineWhiteNextMove(moves)
+
+        assert.equal(firstMove, "Bxg4")
+    });
+    it('plays the entire refutation', function () {
+        const staffordEngine = staffordGameEngine();
+        const moves: MoveEvent[] = [
+            {
+                color: "b",
+                san: "e5"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Nf6"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Nc6"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "dxc6"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Bc5"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Ng4"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Qh4"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Qxg4"
+            } as MoveEvent,
+            {
+                color: "b",
+                san: "Bxg4"
+            } as MoveEvent,
+        ];
+        staffordEngine.determineWhiteNextMove([]);
+        staffordEngine.determineWhiteNextMove([moves[0]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4], moves[5]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4], moves[5], moves[6]]);
+
+        const firstMove = staffordEngine.determineWhiteNextMove(moves)
+
+        assert.equal(firstMove, "Qxg4")
+    });
+})
+describe('knows the oh no my queen trap', function () {
+    let moves: MoveEvent[] = [
+        {
+            color: "b",
+            san: "e5"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "Nf6"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "Nc6"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "dxc6"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "Bc5"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "Nxe4"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "Bxf2"
+        } as MoveEvent,
+        {
+            color: "b",
+            san: "Bg4"
+        } as MoveEvent
+    ] as const;
+    it('plays e4 on first move', function () {
+        const staffordEngine = staffordGameEngine(0.4);
+
+        const firstMove = staffordEngine.determineWhiteNextMove([moves[0]])
+
+        assert.equal(firstMove, "e4")
+    });
+    it('plays Nf3 on second move', function () {
+        const staffordEngine = staffordGameEngine(0.4);
+        const secondMove = [
+            {
+                after: "rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2",
+                before: "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1",
+                color: "b",
+                flags: "w",
+                from: "e7",
+                lan: "e2e4",
+                piece: "p",
+                san: "e5",
+                to: "e5",
+            } as MoveEvent
+        ];
+        staffordEngine.determineWhiteNextMove([]);
+
+        const firstMove = staffordEngine.determineWhiteNextMove(secondMove)
+
+        assert.equal(firstMove, "Nf3")
+    });
+    it('plays Bxg4 on seventh move', function () {
+        const staffordEngine = staffordGameEngine(0.4);
+        staffordEngine.determineWhiteNextMove([]);
+        staffordEngine.determineWhiteNextMove([moves[0]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4]]);
+
+        const firstMove = staffordEngine.determineWhiteNextMove(moves)
+
+        assert.equal(firstMove, "Bxd8")
+    });
+    it('plays the entire refutation', function () {
+        const staffordEngine = staffordGameEngine(0.4);
+        staffordEngine.determineWhiteNextMove([]);
+        staffordEngine.determineWhiteNextMove([moves[0]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4]]);
+        staffordEngine.determineWhiteNextMove([moves[0], moves[1], moves[2], moves[3], moves[4], moves[5]]);
+
+        const firstMove = staffordEngine.determineWhiteNextMove(moves)
+
+        assert.equal(firstMove, "Ke2")
     });
 });
